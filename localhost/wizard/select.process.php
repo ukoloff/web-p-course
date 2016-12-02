40038ae4e0d1477d8ba2dfbcb76a02b9294d0ce5
Stas Ukolov <ukoloff@gmail.com>
2016-12-02 17:05:50 +0500

    Записать результаты голосования

--- localhost/wizard/select.process.php
+++ localhost/wizard/select.process.php
@@ -2,5 +2,8 @@
 if($_POST['action']):
   $_SESSION['president'] = $_POST['action'];
   nextPage('exitpoll');
+  $f = fopen('.data/golos.log', 'a');
+  fputs($f, "${_POST['action']}:${_SERVER['REMOTE_ADDR']}:${_SERVER['HTTP_USER_AGENT']}\n");
+  fclose($f);  
 endif;
 ?>
