4ba2d453b0d23d6d1cc01492e5b46c600158a183
Stas Ukolov <ukoloff@gmail.com>
2016-12-02 17:18:31 +0500

    Показать результаты голосования

--- localhost/wizard/exitpoll.render.php
+++ localhost/wizard/exitpoll.render.php
@@ -1,1 +1,18 @@
 Ваш выбор: <?= $_SESSION['president'] ?>
+
+<h2>Результаты голосования</h2>
+<?
+$f = fopen('.data/golos.log', 'r');
+
+while(!feof($f)):
+	$s = fgets($f);
+	$line = explode(':', $s);
+    $votes[$line[0]]++;
+endwhile;
+
+foreach($votes as $k=>$v):
+?>
+<li><b><?= $k ?></b>: <?= $v ?>
+<?
+endforeach;	
+?>
