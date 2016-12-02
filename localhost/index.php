cd7a2644293ad1156fb5b5df373a52d5055de606
Stas Ukolov <ukoloff@gmail.com>
2016-12-02 16:49:15 +0500

    Показать все файлы/папки

--- localhost/index.php
+++ localhost/index.php
@@ -1,5 +1,10 @@
 <h1>Веб-программирование</h1>
 
+<?
+foreach(glob('*') as $f):
+	echo "<li>", $f;
+endforeach;
+?>
 <li><a href=start>Первая страница</a>
 <li><a href=count>Счётчики</a>
 <li><a href=wizard>Мастер</a>
