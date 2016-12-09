b8bdc7e48a3a0b3988bacdacc2ea62426547e6f6
Stas Ukolov <ukoloff@gmail.com>
2016-12-09 16:24:57 +0500

    Manual quote

--- localhost/wizard/select.db.php
+++ localhost/wizard/select.db.php
@@ -1,3 +1,8 @@
 <?
-  $db = new SQLite3('.data/votes.sq3');
+$db = new SQLite3('.data/votes.sq3');
+
+function q3($s)
+{
+  return str_replace("'", "''", $s);
+}
 ?>
--- localhost/wizard/select.process.php
+++ localhost/wizard/select.process.php
@@ -4,14 +4,17 @@ include('select.db.php');
 if($_POST['action']):
   $_SESSION['president'] = $_POST['action'];
   nextPage('exitpoll');
+  $whom = q3($_POST['action']);
+  $ip = q3($_SERVER["REMOTE_ADDR"]);
+  $ua = q3($_SERVER["HTTP_USER_AGENT"]);
   $db->exec(<<<SQL
   Insert Into
     logs(ctime, whom, ip, ua)
   Values(
     datetime('now'),
-    '${_POST['action']}',
-    '${_SERVER["REMOTE_ADDR"]}',
-    '${_SERVER["HTTP_USER_AGENT"]}')
+    '$whom',
+    '$ip',
+    '$ua')
 SQL
   );
 endif;
